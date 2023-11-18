7d8f6697a69ba20c4a825dabc2bf5d9132eabbed
Winnery05 <elmedhassan2002@gmail.com>
2023-11-19 00:02:57 +0100

    fixe search

--- src/jsx/header.jsx
+++ src/jsx/header.jsx
@@ -2,23 +2,22 @@ import { useState } from "react";
 import {  MdManageSearch1,SiThemoviedatabase1} from '../icons'
 
 export function Header() {
-  const [toSearch, setToSearch] = useState();
+  const [toSearch, setToSearch] = useState('');
+
   return (
     <div className="header">
-      <span className="h-full text-6xl hover:scale-105 " > <SiThemoviedatabase1 /></span>
+      <span>🍿 YOUR MOVIE APP</span>
       <div className="relative">
         <input
-          className="hover:scale-x-105"
-          onChange={(e) =>( setToSearch(e.target.value))}
+          onChange={(e) => setToSearch(e.target.value)}
           type="text"
-          placeholder="serache a movie"
+          placeholder="Search for a movie"
         />
-        <span className=" absolute right-0 top-0 grid place-items-center  w-8 h-full text-2xl text-white">
-          {" "}
+        <span className="absolute right-0 top-0 grid place-items-center w-8 h-full text-2xl text-white">
           <MdManageSearch1 className="icon" />
         </span>
       </div>
-      <span>{toSearch ? { toSearch } : "item finded"}</span>
+      <span>{toSearch ? `Searching for: ${toSearch}` : "No item found"}</span>
     </div>
   );
 }
