7a315ac3f47748ef2fab1e4f525b3311706a68c0
Winnery05 <elmedhassan2002@gmail.com>
2023-11-20 12:41:09 +0100

    second day

--- src/jsx/header.jsx
+++ src/jsx/header.jsx
@@ -5,11 +5,11 @@ export function Header() {
   const [toSearch, setToSearch] = useState('');
 
   return (
-    <div className="header m-2 mb-0">
+    <div className="header m-1 mb-0 p-3 rounded-xl text-white bg-bleuM-100 flex justify-between items-center font-bold">
       <span className="h-full text-6xl hover:scale-105 " > <SiThemoviedatabase1 /></span>
       <div className="relative mx-2">
         <input
-          className="p-2 rounded-xl outline-none  border-none focus:border-none focus:outline-none focus:p-2 pe-[30px] font-bold text-white bg-Secondbm"
+          className="p-2 rounded-xl outline-none  border-none focus:border-none focus:outline-none focus:p-2 pe-[30px] focus:pe-[30px] font-bold text-white bg-Secondbm"
           onChange={(e) => setToSearch(e.target.value)}
           type="text"
           placeholder="Search for a movie"
