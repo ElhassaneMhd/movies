e54275b04c1b7cacc7ad049bdd7f1a9d48c4ecb0
Winnery05 <elmedhassan2002@gmail.com>
2023-12-07 13:57:56 +0100

    fix small screen wraping

--- src/jsx/header.jsx
+++ src/jsx/header.jsx
@@ -10,26 +10,31 @@ export function Header({toSearch,finded,getData,setMovieData, setToSearch}) {
     </div>
   );
 }
-function InputToSearch({setMovieData,setToSearch,getData}) {
+function InputToSearch({ setMovieData, setToSearch, getData }) {
+  function handlSumit(e) {
+    e.preventDefault()
+    getData()
+  }
   return (
-    <div className="relative mx-2">
+    <form onSubmit={(e)=>handlSumit(e)} className="group relative mx-2 w-[70%]">
       <input
         autoFocus={true}
           maxLength={50}
-          className="p-2 rounded-xl outline-none  border-none focus:border-none focus:outline-none focus:p-2 pe-[30px] focus:pe-[30px] font-bold text-white bg-Secondbm"
+          className="peer p-2 rounded-xl  outline-none  border-none focus:scale-x-105 transition duration-500 focus:border-none w-[100%] focus:outline-none focus:p-2 pe-[30px] focus:pe-[30px] font-bold text-white bg-Secondbm"
         onChange={(e) => {
           e.target.value.length >= 2
           ? setToSearch(e.target.value.trim())
-          :(setMovieData([]) && setToSearch(e.target.value.trim()) )
+            : (setToSearch(e.target.value.trim()) )
           e.target.value.length>=4&&getData()
+          e.target.value.length<2&& setMovieData([])
         }}
           type="text"
           placeholder="Search for a movie"
         />
-        <span onClick={()=>  getData()}  className="absolute right-0 top-0 grid place-items-center w-8 h-full text-2xl hover:scale-125 text-white cursor-pointer">
+        <span onClick={()=>  getData()}  className="absolute right-0 top-0 grid place-items-center w-8 h-full text-2xl hover:scale-125 text-white cursor-pointer transition duration-500 peer-focus:translate-x-2">
           <MdManageSearch1 className="icon " />
         </span>
-      </div>
+      </form>
   )
 }
 function Logo() {
@@ -39,9 +44,9 @@ function Logo() {
 }
 function FindedResults({toSearch,finded}) {
   return (
-    <div className='flex flex-col justify-end'>
-      <span className="hidden md:inline-block">{toSearch ? `Searching for: ${toSearch}` : " item found"}</span>
-      <span className="hidden md:inline-block">{finded.length>0 && `${finded.length} item finded`}</span>
+    <div className=' flex-col justify-end hidden md:flex w-fit text-right'>
+      <span >{toSearch ? ` ${toSearch}` : " item found"}</span>
+      <span >{finded.length>0 && `${finded.length} item finded`}</span>
     </div>
   )
 }
